24a1660980e4e76a11abb4fdf0a710a8543b4537
Rafael Neri <rafepel@gmail.com>
2017-10-31 09:58:37 -0300

    Uptade with production.

--- views/header.php
+++ views/header.php
@@ -8,11 +8,12 @@
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,700,900' rel='stylesheet' type='text/css'>
 
     <!-- styles -->
+    <?php $var = "c2"; ?>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.theme.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.13/semantic.min.css"/>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.11.1/sweetalert2.min.css"/> -->
-    <link rel="stylesheet" href="/styles/main.css"/>
+    <link rel="stylesheet" href="/styles/main.css?v=<?= $var ?>"/>
 </head>
 <body>
